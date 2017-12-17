0160e8f5a9b262d79896893a92347ce35c323c24
nilesh <cnilesh.1990@gmail.com>
2017-12-17 20:09:02 +0000

    Changing excel library

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var restify = require('restify');
 var builder = require('botbuilder');
-var parseXlsx = require('excel');
+//var parseXlsx = require('excel');
+//var Excel = require('exceljs');
+var XLSX = require('xlsx');
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -20,14 +22,15 @@ server.post('/api/messages', connector.listen());
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
     //session.send('Hi');
-    readExcel(session.message.text,function (responseData) {
+    read(session.message.text,function (responseData) {
         session.send("Your WBS Code for the current period is: %s", responseData);
     });
+    
  });
 
-var readExcel = function (eid, callback) {
+/*var readExcel = function (eid, callback) {
     console.log(eid);
-    parseXlsx('./wbs_format.xlsx', function (err, data) {
+    parseXlsx('https://pmochatbot.scm.azurewebsites.net/dev/api/files/wwwroot/wbs_format.xlsx', function (err, data) {
         if (err)
             return console.log(err);
         if (data.length === 0) {
@@ -42,4 +45,25 @@ var readExcel = function (eid, callback) {
             }
         }
     });
+}*/
+
+var read = function (eid,callback) {
+    var workbook = XLSX.readFile('wbs_format.xlsx');
+    var sheet_name_list = workbook.SheetNames;
+    //console.log(sheet_name_list);
+    var text = XLSX.utils.sheet_to_csv(workbook.Sheets[sheet_name_list[0]]);
+    var textArr = [];
+    textArr = text.split(/\r?\n/);
+    for (var i = 0; i < textArr.length; i++) {
+        var tempArr = textArr[i].split(',');
+        //console.log(tempArr);
+        if (tempArr[2] == eid) {
+            callback(tempArr[7] + ' for the project:' + tempArr[5]);
+        }
+
+    }
+
+    // pipe from stream
+    //var workbook = new Excel.Workbook();
+    //stream.pipe(workbook.xlsx.createInputStream());
 }
